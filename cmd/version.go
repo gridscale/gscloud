6eb62490fd8d4b98555190d6df962fda5fc1230c
c0deN <patrick@gridscale.io>
2020-04-20 17:05:56 +0200

    Changed text for gscloud --help
    gscloud version displays latest Git commit SHA and Version number.
    
    For example:
    ./gscloud version
    Version:        0.2.0-beta
    Git commit:     66a9631ed5c1516d34ca305d4432149b67675cd0
    
    https://gridscale.atlassian.net/browse/GD-2675

--- cmd/version.go
+++ cmd/version.go
@@ -6,22 +6,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// GitCommit gets its vlaue later
+// GitCommit gets its value through Makefile
 var GitCommit string
 
-// Version gets its value later
+// Version gets its value through Makefile
 var Version string
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Returns last Git Commit and Version",
+	Long: `gscloud version displays latest Git commit SHA and Version number.
+	
+For example:
+./gscloud version
+Version:        0.2.0-beta
+Git commit:     66a9631ed5c1516d34ca305d4432149b67675cd0`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("Version:\t%s\nGit commit:\t%s\n", Version, GitCommit)
 	},
