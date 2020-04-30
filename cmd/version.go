fb95801046b08bcb660f825b7d156fc581003e8a
Benjamin Kircher <bkircher@gridscale.io>
2020-05-01 00:01:13 +0200

    version cmd: hide some flags
    Hide some global persistent flags here that don't make sense on
    'version' and make help texts a little shorter.

--- cmd/version.go
+++ cmd/version.go
@@ -6,22 +6,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// GitCommit gets its value through Makefile
+// GitCommit value set by a linker flag
 var GitCommit string
 
-// Version gets its value through Makefile
+// Version value set by a linker flag
 var Version string
 
-// versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Returns last Git Commit and Version",
-	Long: `gscloud version displays latest Git commit SHA and Version number.
-	
-For example:
-./gscloud version
-Version:        0.2.0-beta
-Git commit:     66a9631ed5c1516d34ca305d4432149b67675cd0`,
+	Short: "Print the version",
+	Long:  `Display gscloud version information.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("Version:\t%s\nGit commit:\t%s\n", Version, GitCommit)
 	},
@@ -29,4 +23,14 @@ Git commit:     66a9631ed5c1516d34ca305d4432149b67675cd0`,
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+
+	// Hide some global persistent flags here that don't make sense on 'version'
+	origHelpFunc := versionCmd.HelpFunc()
+	rootCmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		if cmd.Name() == "version" || (cmd.Parent() != nil && cmd.Parent().Name() == "version") {
+			cmd.Flags().MarkHidden("account")
+			cmd.Flags().MarkHidden("config")
+		}
+		origHelpFunc(cmd, args)
+	})
 }
