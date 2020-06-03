114557a8ce485ca04c7851def51e96fe7fde2757
c0deN <patrick@gridscale.io>
2020-06-03 11:07:59 +0200

    refactor tablerendering, add new func AsJSON

--- render/render_test.go
+++ render/render_test.go
@@ -1,4 +1,4 @@
-package tablerendering
+package render
 
 import (
 	"bytes"
@@ -10,7 +10,7 @@ import (
 
 func TestWithByteBuffer(t *testing.T) {
 	out := new(bytes.Buffer)
-	RenderTable(out, []string{"test", "version", "text"}, [][]string{{"1", "version 1", "empty"}, {"a2", "b2", "c3"}})
+	Table(out, []string{"test", "version", "text"}, [][]string{{"1", "version 1", "empty"}, {"a2", "b2", "c3"}})
 
 	countedLines := strings.Count(out.String(), "\n")
 	assert.Equal(t, countedLines, 3)
