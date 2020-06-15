78388d4c29a0bdec17a96fb5846e7784990b12b7
c0deN <patrick@gridscale.io>
2020-06-15 15:38:10 +0200

    Turn server on and off via UUID
    $ gscloud server on <id>
    $ gscloud server off <id>
    $ gscloud server off --force <id>
    
    Fixes https://github.com/gridscale/gscloud/issues/31

--- cmd/server.go
+++ cmd/server.go
@@ -10,13 +10,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var forceFlag bool
+
 var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Print server list",
 	Long:  `Print all server information`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := context.Background()
-
 		out := new(bytes.Buffer)
 		servers, err := client.GetServerList(ctx)
 		if err != nil {
@@ -40,13 +41,11 @@ var serverCmd = &cobra.Command{
 					},
 				}
 				serverinfos = append(serverinfos, fill...)
-
 			}
 			if idFlag {
 				rowsToDisplay = len(heading)
 			}
 			render.Table(out, heading[:rowsToDisplay], serverinfos)
-
 		} else {
 			render.AsJSON(out, servers)
 		}
@@ -54,6 +53,29 @@ var serverCmd = &cobra.Command{
 	},
 }
 
+var onCmd, offCmd = &cobra.Command{
+	Use:   "on",
+	Short: "Turn server on",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
+		client.StartServer(ctx, args[0])
+	},
+}, &cobra.Command{
+	Use:   "off",
+	Short: "Turn server off",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
+		if forceFlag {
+			client.StopServer(ctx, args[0])
+		}
+		client.ShutdownServer(ctx, args[0])
+	},
+}
+
 func init() {
+	serverCmd.AddCommand(onCmd, offCmd)
+	serverCmd.PersistentFlags().BoolVarP(&forceFlag, "force", "f", false, "Force shutdown")
 	rootCmd.AddCommand(serverCmd)
 }
