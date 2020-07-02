ba03dbc0e057e20d4500dab53e681c3eb38e9224
Van Thong Nguyen <tom@gridscale.io>
2020-07-02 11:02:48 +0200

    implement `produceStorageCmdFunc` which returns `cmdFunc`
    This will allow us to implement a mocking `storageGetter` which is used for testing purpose

--- cmd/storage.go
+++ cmd/storage.go
@@ -18,47 +18,52 @@ type storageGetter interface {
 	GetStorageList(ctx context.Context) ([]gsclient.Storage, error)
 }
 
+// produceStorageCmdRunFunc takes an instance of a struct that implements `storageGetter`
+// returns a `cmdRunFun`
+func produceStorageCmdRunFunc(g storageGetter) cmdRunFunc {
+	return func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
+		out := new(bytes.Buffer)
+		storages, err := client.GetStorageList(ctx)
+		if err != nil {
+			log.Error("Couldn't get Storageinfo", err)
+			return
+		}
+		var storageinfo [][]string
+		if !jsonFlag {
+			heading := []string{"id", "name", "capacity", "changetime", "status"}
+			for _, stor := range storages {
+				fill := [][]string{
+					{
+						stor.Properties.ObjectUUID,
+						stor.Properties.Name,
+						strconv.FormatInt(int64(stor.Properties.Capacity), 10),
+						strconv.FormatInt(int64(stor.Properties.ChangeTime.Hour()), 10),
+						stor.Properties.Status,
+					},
+				}
+				storageinfo = append(storageinfo, fill...)
+			}
+			if quietFlag {
+				for _, info := range storageinfo {
+					fmt.Println(info[0])
+				}
+				return
+			}
+			render.Table(out, heading[:], storageinfo)
+		} else {
+			render.AsJSON(out, storages)
+		}
+		fmt.Print(out)
+	}
+}
+
 func init() {
 	var storageCmd = &cobra.Command{
 		Use:   "storage",
 		Short: "Print storage list",
 		Long:  `Print all storage information`,
-		Run: func(cmd *cobra.Command, args []string) {
-			ctx := context.Background()
-			out := new(bytes.Buffer)
-			storages, err := client.GetStorageList(ctx)
-			if err != nil {
-				log.Error("Couldn't get Storageinfo", err)
-				return
-			}
-			var storageinfo [][]string
-			if !jsonFlag {
-				heading := []string{"id", "name", "capacity", "changetime", "status"}
-				for _, stor := range storages {
-					fill := [][]string{
-						{
-							stor.Properties.ObjectUUID,
-							stor.Properties.Name,
-							strconv.FormatInt(int64(stor.Properties.Capacity), 10),
-							strconv.FormatInt(int64(stor.Properties.ChangeTime.Hour()), 10),
-							stor.Properties.Status,
-						},
-					}
-					storageinfo = append(storageinfo, fill...)
-				}
-				if quietFlag {
-					for _, info := range storageinfo {
-						fmt.Println(info[0])
-					}
-					return
-				}
-				render.Table(out, heading[:], storageinfo)
-			} else {
-				render.AsJSON(out, storages)
-			}
-			fmt.Print(out)
-		},
+		Run:   produceStorageCmdRunFunc(client),
 	}
-
 	rootCmd.AddCommand(storageCmd)
 }
