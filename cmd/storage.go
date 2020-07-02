15483b894d2db3acd06f44b5a82bf0c83f8c2791
Van Thong Nguyen <tom@gridscale.io>
2020-07-02 10:47:59 +0200

    move storageCmd variable to the init func
    There are no reasons to make this variable global

--- cmd/storage.go
+++ cmd/storage.go
@@ -18,48 +18,47 @@ type storageGetter interface {
 	GetStorageList(ctx context.Context) ([]gsclient.Storage, error)
 }
 
-var storageCmd = &cobra.Command{
-	Use:   "storage",
-	Short: "Print storage list",
-	Long:  `Print all storage information`,
-	Run: func(cmd *cobra.Command, args []string) {
-		ctx := context.Background()
-		out := new(bytes.Buffer)
-		storages, err := client.GetStorageList(ctx)
-		if err != nil {
-			log.Error("Couldn't get Storageinfo", err)
-			return
-		}
-		var storageinfo [][]string
-		if !jsonFlag {
-			heading := []string{"id", "name", "capacity", "changetime", "status"}
-			for _, stor := range storages {
-				fill := [][]string{
-					{
-						stor.Properties.ObjectUUID,
-						stor.Properties.Name,
-						strconv.FormatInt(int64(stor.Properties.Capacity), 10),
-						strconv.FormatInt(int64(stor.Properties.ChangeTime.Hour()), 10),
-						stor.Properties.Status,
-					},
-				}
-				storageinfo = append(storageinfo, fill...)
-			}
-			if quietFlag {
-				for _, info := range storageinfo {
-					fmt.Println(info[0])
-				}
+func init() {
+	var storageCmd = &cobra.Command{
+		Use:   "storage",
+		Short: "Print storage list",
+		Long:  `Print all storage information`,
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx := context.Background()
+			out := new(bytes.Buffer)
+			storages, err := client.GetStorageList(ctx)
+			if err != nil {
+				log.Error("Couldn't get Storageinfo", err)
 				return
 			}
-			render.Table(out, heading[:], storageinfo)
+			var storageinfo [][]string
+			if !jsonFlag {
+				heading := []string{"id", "name", "capacity", "changetime", "status"}
+				for _, stor := range storages {
+					fill := [][]string{
+						{
+							stor.Properties.ObjectUUID,
+							stor.Properties.Name,
+							strconv.FormatInt(int64(stor.Properties.Capacity), 10),
+							strconv.FormatInt(int64(stor.Properties.ChangeTime.Hour()), 10),
+							stor.Properties.Status,
+						},
+					}
+					storageinfo = append(storageinfo, fill...)
+				}
+				if quietFlag {
+					for _, info := range storageinfo {
+						fmt.Println(info[0])
+					}
+					return
+				}
+				render.Table(out, heading[:], storageinfo)
+			} else {
+				render.AsJSON(out, storages)
+			}
+			fmt.Print(out)
+		},
+	}
 
-		} else {
-			render.AsJSON(out, storages)
-		}
-		fmt.Print(out)
-	},
-}
-
-func init() {
 	rootCmd.AddCommand(storageCmd)
 }
