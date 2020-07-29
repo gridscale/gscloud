808d70dd56207ee28989b5b5d33b0b9f05fc62d0
c0deN <patrick@gridscale.io>
2020-07-29 08:36:28 +0200

    - Change some string text
    - Remove path flag
    
    - Add first argument instead

--- cmd/manpage.go
+++ cmd/manpage.go
@@ -7,8 +7,6 @@ import (
 	"log"
 )
 
-var manpagePath string
-
 // Manpage action enum
 const (
 	manpageCreateAction = iota
@@ -20,17 +18,16 @@ func produceManpageCmdRunFunc(action int) cmdRunFunc {
 	switch action {
 	case manpageCreateAction:
 		return func(cmd *cobra.Command, args []string) {
-
 			header := &doc.GenManHeader{
 				Title:   "GSCLOUD",
 				Section: "1",
 				Source:  "Copyright (c) 2020 gridscale GmbH",
 			}
-			fmt.Print(manpagePath)
-			err := doc.GenManTree(rootCmd, header, manpagePath)
+			err := doc.GenManTree(rootCmd, header, args[0])
 			if err != nil {
 				log.Fatalf("Couldn't create Manpage: %s", err)
 			}
+			fmt.Println("Manpages created:", args[0])
 		}
 	default:
 	}
@@ -42,10 +39,8 @@ func initManpageCmd() {
 		Use:     "manpage [flags]",
 		Short:   "Add Manpage",
 		Long:    `Create manpage in given path.`,
-		Example: `./gscloud manpage --path "/path/to/your/manpages`,
+		Example: `./gscloud manpage /path/to/manpages`,
 		Run:     produceManpageCmdRunFunc(manpageCreateAction),
 	}
-	manpageCreateCmd.PersistentFlags().StringVarP(&manpagePath, "path", "p", "", "Export Manpage to given path")
-
 	rootCmd.AddCommand(manpageCreateCmd)
 }
