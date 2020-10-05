7bbeca45585063b44a84d7c0a42cdb418f3ca34d
Vinicius Zavam <vinicius@gridscale.io>
2020-10-05 11:40:43 +0200

    Simplify echoed error msg, when "CHANGELOG" file isnt present

--- .githooks/pre-push/check-changelog.sh
+++ .githooks/pre-push/check-changelog.sh
@@ -17,6 +17,6 @@ if [ ! -z $has_CHANGELOG ]; then
     echo $GIT_TAG_COMMIT | grep -v $GIT_COMMIT_SHORT ) >/dev/null && exit 0 || exit 123
   ( grep "${DATE}" $CHANGELOG ) || exit 123
 else
-  echo "Please tag this release, and rework CHANGELOG.md to add a date."
+  echo "No ${CHANGELOG} found."
   exit 234
 fi
