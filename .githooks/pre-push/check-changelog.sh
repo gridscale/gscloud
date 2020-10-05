86898539c0b1ad4bebbde0b19424fca9a004ec14
Vinicius Zavam <vinicius@gridscale.io>
2020-10-05 11:42:29 +0200

    Replace test check from "! -z" by "-n"

--- .githooks/pre-push/check-changelog.sh
+++ .githooks/pre-push/check-changelog.sh
@@ -12,7 +12,7 @@ GIT_TAG_COMMIT=$($GIT_CMD describe --abbrev=7 --tags)
 ############################################################
 CHANGELOG="CHANGELOG.md"
 has_CHANGELOG=$($GIT_CMD ls-tree --full-tree -r --name-only HEAD | grep -i $CHANGELOG)
-if [ ! -z $has_CHANGELOG ]; then
+if [ -n $has_CHANGELOG ]; then
   ( grep $GIT_TAG $CHANGELOG && \
     echo $GIT_TAG_COMMIT | grep -v $GIT_COMMIT_SHORT ) >/dev/null && exit 0 || exit 123
   ( grep "${DATE}" $CHANGELOG ) || exit 123
