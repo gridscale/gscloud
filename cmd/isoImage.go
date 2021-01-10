c8bc061c7cd47450ec4c8e84e2ed0a0de4336917
Benjamin Kircher <bkircher@gridscale.io>
2021-01-10 19:16:29 +0100

    Implement gscloud-iso-image-{create,rm}
    Fixes #101

--- cmd/isoImage.go
+++ cmd/isoImage.go
@@ -6,13 +6,15 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/gridscale/gsclient-go/v3"
 	"github.com/gridscale/gscloud/render"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 type isoImageCmdFlags struct {
-	name string
+	name      string
+	sourceURL string
 }
 
 var (
@@ -73,7 +75,55 @@ var isoImageLsCmd = &cobra.Command{
 	},
 }
 
+var isoImageRmCmd = &cobra.Command{
+	Use:     "rm [ID]",
+	Aliases: []string{"remove"},
+	Short:   "Remove ISO image",
+	Long:    `Remove an existing ISO image.`,
+	Args:    cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		imageOp := rt.ISOImageOperator()
+		ctx := context.Background()
+		err := imageOp.DeleteISOImage(ctx, args[0])
+		if err != nil {
+			log.Fatalf("Removing ISO image failed: %s", err)
+		}
+	},
+}
+
+var isoImageCreateCmd = &cobra.Command{
+	Use:   "create [flags]",
+	Short: "Create a private ISO image",
+	Long: `Create a new private ISO image.
+
+# EXAMPLES
+
+Create a Fedora CoreOS image:
+
+	$ gscloud iso-image create \
+	   --name="Fedora CoreOS" \
+	   --source-url="https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/33.20201214.3.1/x86_64/fedora-coreos-33.20201214.3.1-live.x86_64.iso"
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		imageOp := rt.ISOImageOperator()
+		ctx := context.Background()
+		image, err := imageOp.CreateISOImage(ctx, gsclient.ISOImageCreateRequest{
+			Name:      isoImageFlags.name,
+			SourceURL: isoImageFlags.sourceURL,
+		})
+		if err != nil {
+			log.Fatalf("Creating ISO image failed: %s", err)
+		}
+		fmt.Println("Image created:", image.ObjectUUID)
+	},
+}
+
 func init() {
-	isoImageCmd.AddCommand(isoImageLsCmd)
+	isoImageCreateCmd.Flags().StringVar(&isoImageFlags.name, "name", "", "Name of the image")
+	isoImageCreateCmd.MarkFlagRequired("name")
+	isoImageCreateCmd.Flags().StringVar(&isoImageFlags.sourceURL, "source-url", "", "URL from where the image is downloaded")
+	isoImageCreateCmd.MarkFlagRequired("source-url")
+
+	isoImageCmd.AddCommand(isoImageLsCmd, isoImageRmCmd, isoImageCreateCmd)
 	rootCmd.AddCommand(isoImageCmd)
 }
