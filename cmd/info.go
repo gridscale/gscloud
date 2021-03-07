ddfa0b043de74b1c838913671583286bb5be7a40
Van Thong Nguyen <tom@gridscale.io>
2021-03-07 19:51:37 +0100

    print info in table/json format

--- cmd/info.go
+++ cmd/info.go
@@ -1,12 +1,23 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"strconv"
 
+	"github.com/gridscale/gscloud/render"
 	"github.com/gridscale/gscloud/runtime"
 	"github.com/spf13/cobra"
 )
+
+type infoJSONOutput struct {
+	runtime.AccountEntry
+	ServerCount  int `json:"server_count"`
+	StorageCount int `json:"storage_count"`
+	IPAddrCount  int `json:"ip_address_count"`
+	PaaSCount    int `json:"paas_service_count"`
+}
 
 var infoCmd = &cobra.Command{
 	Use:   "info",
@@ -21,6 +32,25 @@ var infoCmd = &cobra.Command{
 			accountName := rt.Account()
 			// get info of the current account
 			if account.Name == accountName {
+				// Print auth info
+				if !rootFlags.json {
+					out := new(bytes.Buffer)
+					heading := []string{"Name", "UserID", "Token", "URL"}
+					fill := [][]string{
+						{
+							account.Name,
+							account.UserID,
+							account.Token,
+							account.URL,
+						},
+					}
+					var rows [][]string
+					rows = append(rows, fill...)
+					render.AsTable(out, heading, rows, renderOpts)
+					fmt.Print(out)
+					fmt.Printf("\nGetting infomation about available resources...\n\n\n")
+				}
+
 				// Get info about primitive resources
 				client := rt.Client()
 				servers, err := client.GetServerList(context.Background())
@@ -39,14 +69,32 @@ var infoCmd = &cobra.Command{
 				if err != nil {
 					return NewError(cmd, "Could not get PaaS services' information", err)
 				}
-				fmt.Printf(
-					"Account: %s\nUserID: %s\nToken: %s\nURL: %s\n",
-					account.Name, account.UserID, account.Token, account.URL)
-				fmt.Printf(
-					"No. of servers: %d\nNo. of storages: %d\nNo. of ip addresses: %d\nNo. of platform services: %d\n",
-					len(servers), len(storages), len(ipAddrs), len(paasServices))
+				out := new(bytes.Buffer)
+				if !rootFlags.json {
+					heading := []string{"No. of servers", "No. of storages", "No. of ip addresses", "No. of PaaS services"}
+					fill := [][]string{
+						{
+							strconv.Itoa(len(servers)),
+							strconv.Itoa(len(storages)),
+							strconv.Itoa(len(ipAddrs)),
+							strconv.Itoa(len(paasServices)),
+						},
+					}
+					var rows [][]string
+					rows = append(rows, fill...)
+					render.AsTable(out, heading, rows, renderOpts)
+				} else {
+					jsonOutput := infoJSONOutput{
+						AccountEntry: account,
+						ServerCount:  len(servers),
+						StorageCount: len(storages),
+						IPAddrCount:  len(ipAddrs),
+						PaaSCount:    len(paasServices),
+					}
+					render.AsJSON(out, jsonOutput)
+				}
+				fmt.Print(out)
 			}
-
 		}
 		return nil
 	},
