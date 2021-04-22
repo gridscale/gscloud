05b67674fd4e533352037ad760cc45e9f22a427e
Felix Kronlage-Dammers <fkr@hazardous.org>
2021-04-22 06:14:35 +0200

    sprinkle error handling

--- release-src.sh
+++ release-src.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+error() {
+	echo ""
+	echo "$1"
+	echo ""
+	exit 1
+}
+
+if [[ $# -eq 0 ]] ; then
+	error "Submit a valid git tag as an argument."
+fi
+
 REPO=https://github.com/gridscale/gscloud.git
 VERSION=`git describe --tags`
 GIT_COMMIT=`git rev-list -1 HEAD`
@@ -10,8 +21,17 @@ RELDIR=$(mktemp -d)
 cd $RELDIR
 
 git clone $REPO
+
+if [[ $? -ne 0 ]]; then
+	error "Error cloning repo"
+fi
+
 cd gscloud
 git checkout $1
+
+if [[ $? -ne 0 ]]; then
+	error "Error switching to supplied git tag"
+fi
 
 sed -e "s/VERSION=\$\$(git describe --tags)/VERSION=${VERSION}/g" -e "s/GIT_COMMIT=\$\$(git rev-list -1 HEAD)/GIT_COMMIT=${GIT_COMMIT}/g"  Makefile > Makefile.tmp
 mv Makefile.tmp Makefile
