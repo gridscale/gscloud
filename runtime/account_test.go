4fbe0c37557d4617d9acf033ed3af04cfd26de5c
JanLukasGithub <jan@garloff.de>
2022-06-02 10:02:53 +0200

    Add tests for env variables

--- runtime/account_test.go
+++ runtime/account_test.go
@@ -1,6 +1,8 @@
 package runtime
 
 import (
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +16,36 @@ func Test_SelectAccount(t *testing.T) {
 	rt, err := NewRuntime(testConfig, "test")
 	assert.Nil(t, err)
 	assert.Equal(t, rt.Account(), "test")
+}
+
+func Test_SelectAccountSettings(t *testing.T) {
+	defer resetEnv(os.Environ())
+
+	os.Setenv("GSCLOUD_USER_ID", "passedUserId")
+	os.Setenv("GSCLOUD_API_TOKEN", "passedToken")
+	os.Setenv("GSCLOUD_API_URL", "passed.example.com")
+
+	testAccount := AccountEntry{
+		Name:   "test",
+		UserID: "test",
+		Token:  "test",
+		URL:    "test.example.com",
+	}
+	testConfig := Config{Accounts: []AccountEntry{testAccount}}
+	rt, err := NewRuntime(testConfig, "test")
+
+	assert.Nil(t, err)
+	assert.Equal(t, "passedUserId", rt.config.Accounts[0].UserID)
+	assert.Equal(t, "passedToken", rt.config.Accounts[0].Token)
+	assert.Equal(t, "passed.example.com", rt.config.Accounts[0].URL)
+}
+
+func resetEnv(environ []string) {
+	os.Clearenv()
+
+	for _, s := range environ {
+		splitString := strings.Split(s, "=")
+
+		os.Setenv(splitString[0], splitString[1])
+	}
 }
